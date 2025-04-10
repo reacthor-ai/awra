2eb91a15c4072dc5214aa38fb26a9c411bf29ca3
Williams Sissoko <35783824+wcisco17@users.noreply.github.com>
2025-04-10 14:59:00 -0400

    AWRA-00(feat): redirect login auth

--- src/hooks/use-auth-signout.tsx
+++ src/hooks/use-auth-signout.tsx
@@ -1,27 +1,20 @@
 import { signOut } from 'next-auth/react';
 import { clearGuestData } from "@/lib/guestDb";
 import { useRouter } from "next/navigation";
+import { apiRoutes } from "@/utils/api-links";
+import { navItems } from "@/libs/navigation/nav-items";
+import { navigationLinks } from "@/utils/nav-links";
 
 export const useAuthSignOut = () => {
   const router = useRouter()
   const handleSignOut = async () => {
     try {
-      await Promise.all([
-        fetch('/api/auth/signout', {
-          method: 'POST',
-          credentials: 'include',
-        }),
-        clearGuestData()
-      ])
-
-      await new Promise(resolve => setTimeout(resolve, 250));
-
       const data = await signOut({
-        redirect: false,
+        redirect: true,
         callbackUrl: '/'
       });
 
-      router.push(data.url)
+      router.push(navigationLinks.login())
     } catch (error) {
       console.error('SignOut error:', error);
       window.location.href = '/'
